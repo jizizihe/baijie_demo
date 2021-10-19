bb2f3564a185af9597cb7440b6c201181638c396
liuyu <liuyu@szbaijie.cn>
2021-10-19 10:39:03 +0800

    add eth0set

---
+++ qt/t507/eth0_popup.cpp
@@ -0,0 +1,129 @@
+#include "eth0_popup.h"
+#include <QRegExpValidator>
+#include <QPushButton>
+
+eth0_popup::eth0_popup(QWidget *parent) :
+    QDialog(parent)
+{
+    resize(400,300);
+    QPushButton *remainmenu = new QPushButton("OK",this);
+    remainmenu->setGeometry(900,550,100,40);
+//    edit = new QLineEdit(this);
+//    edit->setGeometry(300,200,300,30);
+
+    label_Name = new QLabel("name:",this);
+    label_Name->setGeometry(300,200,130,40);
+    line_name = new QLineEdit(this);
+    line_name->setGeometry(450,200,200,40);
+
+    label_addr = new QLabel("ip address:",this);
+    label_addr->setGeometry(300,300,130,40);
+    line_addr = new QLineEdit(this);
+    line_addr->setGeometry(450,300,200,40);
+    QRegExp a("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+    line_addr->setValidator(new QRegExpValidator(a,this));
+
+    label_gateway = new QLabel("gateway:",this);
+    label_gateway->setGeometry(300,400,130,40);
+    line_gateway = new QLineEdit(this);
+    line_gateway->setGeometry(450,400,200,40);
+    QRegExp b("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+    line_gateway->setValidator(new QRegExpValidator(b,this));
+
+    connect(remainmenu,SIGNAL(clicked(bool)),this,SLOT(remain()));
+    connect(this,SIGNAL(addip()),this,SLOT(getaddipinforma()));
+    connect(this,SIGNAL(modip()),this,SLOT(getmodipinforma()));
+    connect(this,SIGNAL(delip()),this,SLOT(getdelipinforma()));
+}
+
+eth0_popup::~eth0_popup()
+{
+
+}
+
+void eth0_popup::remain()
+{
+    if("add ip" == this->bnttype)
+    {
+        emit addipinfor(this->line_name->text(),this->line_addr->text(),this->line_gateway->text());
+    }
+    else if("mod ip" == this->bnttype)
+    {
+        emit modipinfor(this->line_name->text(),this->line_addr->text(),this->line_gateway->text());
+    }
+    else if("del ip" == this->bnttype)
+    {
+        emit delipinfor(this->line_name->text());
+    }
+}
+
+void eth0_popup::getaddipinforma()
+{
+    label_addr->setVisible(true);
+    label_gateway->setVisible(true);
+    line_addr->setVisible(true);
+    line_gateway->setVisible(true);
+
+    label_Name->move(300,200);
+    line_name->move(450,200);
+//    label_Name = new QLabel("name:",this);
+//    label_Name->setGeometry(300,200,130,40);
+//    line_name = new QLineEdit(this);
+//    line_name->setGeometry(450,200,200,40);
+
+//    label_addr = new QLabel("ip address:",this);
+//    label_addr->setGeometry(300,300,130,40);
+//    line_addr = new QLineEdit(this);
+//    line_addr->setGeometry(450,300,200,40);
+//    QRegExp a("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+//    line_addr->setValidator(new QRegExpValidator(a,this));
+
+//    label_gateway = new QLabel("gateway:",this);
+//    label_gateway->setGeometry(300,400,130,40);
+//    line_gateway = new QLineEdit(this);
+//    line_gateway->setGeometry(450,400,200,40);
+//    QRegExp b("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+//    line_gateway->setValidator(new QRegExpValidator(b,this));
+}
+
+void eth0_popup::getmodipinforma()
+{
+    label_addr->setVisible(true);
+    label_gateway->setVisible(true);
+    line_addr->setVisible(true);
+    line_gateway->setVisible(true);
+
+    label_Name->move(300,200);
+    line_name->move(450,200);
+//    label_Name = new QLabel("name:",this);
+//    label_Name->setGeometry(300,200,130,40);
+//    line_name = new QLineEdit(this);
+//    line_name->setGeometry(450,200,200,40);
+
+//    label_addr = new QLabel("ip address:",this);
+//    label_addr->setGeometry(300,300,130,40);
+//    line_addr = new QLineEdit(this);
+//    line_addr->setGeometry(450,300,200,40);
+//    QRegExp a("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+//    line_addr->setValidator(new QRegExpValidator(a,this));
+
+//    label_gateway = new QLabel("gateway:",this);
+//    label_gateway->setGeometry(300,400,130,40);
+//    line_gateway = new QLineEdit(this);
+//    line_gateway->setGeometry(450,400,200,40);
+//    QRegExp b("\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+//    line_gateway->setValidator(new QRegExpValidator(b,this));
+}
+
+void eth0_popup::getdelipinforma()
+{
+    label_addr->setVisible(false);
+    label_gateway->setVisible(false);
+    line_addr->setVisible(false);
+    line_gateway->setVisible(false);
+
+    //label_Name = new QLabel("name:",this);
+    label_Name->move(300,300);
+    //line_name = new QLineEdit(this);
+    line_name->move(450,300);
+}
---
+++ qt/t507/eth0set.h
@@ -0,0 +1,54 @@
+#ifndef ETH0SET_H
+#define ETH0SET_H
+
+#include <QWidget>
+#include <QString>
+#include <QProcess>
+#include <QDebug>
+#include <unistd.h>
+#include "eth0_popup.h"
+
+namespace Ui {
+class eth0set;
+}
+
+class eth0set : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit eth0set(QWidget *parent = 0);
+    ~eth0set();
+     eth0_popup popup;
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+    void speedtest();
+    void on_pushButton_9_clicked();
+    void increaseip(QString,QString,QString);
+    void modifyip(QString,QString,QString);
+    void deleteip(QString);
+
+signals:
+    void Mysignal();
+    void StartTestSpeed();
+private:
+    Ui::eth0set *ui;
+
+};
+
+#endif // ETH0SET_H
